582a4a98bf64bfbe1007f09b41c19f5990f6177b
yh <>
2023-07-03 09:08:26 +0900

    [wip]ユーザー登録

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '3.1.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem "rails","7.0.4"
-gem 'puma', '~> 5.0'
+gem 'puma', '~> 5.6.4'
 gem 'sass-rails', '>= 6'
 gem 'webpacker', '~> 5.0'
 gem 'sqlite3'
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -9,19 +9,9 @@ get '/signup/new', to: 'users#new', as: 'signup_new'
 # 基本的にはデータの経由の役割なので専用画面はいらない
 resources :users, only: [:create]
 
-get  "/login/new",  to: "sessions#new", as: 'login_new'
-post "/login/create",  to: "sessions#create"
+get  "/login/new",  to: "sessions#new", as: 'login_mew'
+post "/login/create",  to: "sessions#create",as: 'login_create'
 delete '/logout', to: 'sessions#destroy'
-
-# edit（編集フォーム）
-get '/signup/:id/edit', to: 'users#edit'
-
-# update（更新）
-patch '/signup/:id', to: 'users#update'
-put '/signup/:id', to: 'users#update'
-
-# destroy（削除）
-delete '/signup/:id', to: 'users#destroy'
 
 # show（詳細表示）
 get '/mypage/:id', to: 'mypage#show'
